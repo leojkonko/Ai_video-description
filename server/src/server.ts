edf818857f1d704eb861d0a1144dd94e05433307
leojkonko <leonardolinomeyer@gmail.com>
2023-09-24 20:34:37 -0300

    finishing front and add back server

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+
+const app = fastify()
+
+app.register(getAllPromptsRoute)
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
+
+ 
